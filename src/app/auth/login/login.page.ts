6a248767205621985c5b9536d06018e52a298987
Santiago de la Fuente <santiago.daniel.delafuente@hotmail.com>
2019-04-16 23:24:29 -0300

    Tres. Spinner y estilos.

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -14,28 +14,35 @@ export class LoginPage implements OnInit {
 
   email: string;
   password: string;
+  loading: boolean;
 
   constructor(
     private  router:  Router,
     public afAuth: AngularFireAuth
-  ) { }
+  ) {
+    this.loading = false;
+  }
 
   ngOnInit() {
   }
 
   async login() {
-    // const = { this.email, this.password } = this;
+    this.loading = true;
     try {
+
       const response = await this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password);
-      console.log(response);
+      // console.log(response);
       if (response.user) {
         this.router.navigateByUrl('bienvenido');
       }
 
     } catch (err) {
-        if (err.code == 'auth/user-not-found' ) {
-            console.dir(err);
-        }
+        this.loading = false;
+        // if (err.code === 'auth/user-not-found' ) {
+        //     console.dir(err);
+        // } else {
+        //   console.dir(err);
+        // }
     }
   }
 
