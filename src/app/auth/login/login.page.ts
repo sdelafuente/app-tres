4aea760a6993293c6df2960f413ae0ab2fcfc6a6
Santiago de la Fuente <santiago.daniel.delafuente@hotmail.com>
2019-09-17 23:26:18 -0300

    Tres. Alarma, reusar lo viejo.

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -16,11 +15,50 @@ export class LoginPage implements OnInit {
   password: string;
   loading: boolean;
   splash: boolean;
-  // secondPage = SecondPagePage;
+  validationsForm: FormGroup;
+  matching_passwords_group: FormGroup;
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Last name is required.' }
+    ],
+    'email': [
+      { type: 'required', message: 'El email es necesario para acceder.' },
+      { type: 'pattern', message: 'Ingrese un email valido.' }
+    ],
+    'phone': [
+      { type: 'required', message: 'Phone is required.' },
+      { type: 'validCountryPhone', message: 'The phone is incorrect for the selected country.' }
+    ],
+    'password': [
+      { type: 'required', message: 'La contraseña es necesaria.' },
+      { type: 'minlength', message: 'La contraseña tiene que tener al menos 5 caracteres.' },
+      { type: 'pattern', message: 'La contraseña tiene que tener al menos una mayuscula, un numero y un caracter.' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required.' }
+    ],
+    'matching_passwords': [
+      { type: 'areEqual', message: 'Password mismatch.' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions.' }
+    ],
+  };
 
   constructor(
     private  router:  Router,
-    public afAuth: AngularFireAuth
+    public afAuth: AngularFireAuth,
+    public formBuilder: FormBuilder
   ) {
     this.loading = false;
     this.splash = true;
@@ -29,6 +67,20 @@ export class LoginPage implements OnInit {
   ngOnInit() {
     this.loading = false;
     this.ionViewDidLoad();
+
+    const REGEXP = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    this.validationsForm = this.formBuilder.group({
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP)
+        ])),
+        password: new FormControl('', Validators.compose([
+          Validators.minLength(5),
+          Validators.required,
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+        ]))
+    });
   }
 
   ionViewDidLoad() {
@@ -37,11 +89,12 @@ export class LoginPage implements OnInit {
   }
 
   async login() {
+    console.log('login');
     this.loading = true;
     try {
 
-      const response = await this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password);
-      // console.log(response);
+      const response = await this.afAuth.auth.signInWithEmailAndPassword(this.validationsForm.value.email, this.validationsForm.value.password);
+      console.log(response);
       if (response.user) {
         this.loading = false;
         this.router.navigateByUrl('bienvenido');
@@ -49,43 +102,107 @@ export class LoginPage implements OnInit {
 
     } catch (err) {
         this.loading = false;
-        // if (err.code === 'auth/user-not-found' ) {
-        //     console.dir(err);
-        // } else {
-        //   console.dir(err);
-        // }
+        if (err.code === 'auth/user-not-found' ) {
+            console.dir(err);
+        } else {
+          console.dir(err);
+        }
     }
   }
 
 
 
+  // cargarUsuario(user) {
+  //   switch (user) {
+  //      case 'admin': {
+  //         this.email = 'admin@gmail.com';
+  //         this.password = '111111';
+  //         break;
+  //      }
+  //      case 'invitado': {
+  //        this.email = 'invitado@gmail.com';
+  //        this.password = '222222';
+  //         break;
+  //      }
+  //      case 'usuario': {
+  //        this.email = 'usuario@gmail.com';
+  //        this.password = '333333';
+  //         break;
+  //      }
+  //      case 'D': {
+  //         console.log('Poor');
+  //         break;
+  //      }
+  //      default: {
+  //         console.log('Invalid choice');
+  //         break;
+  //      }
+  //   }
+  // }
+
   cargarUsuario(user) {
+
     switch (user) {
        case 'admin': {
-          this.email = 'admin@gmail.com';
-          this.password = '111111';
-          break;
+           this.validationsForm = this.formBuilder.group({
+             email: ['admin@gmail.com',
+               Validators.compose([Validators.required, Validators.email])],
+             password: [
+              '111111',
+               Validators.compose([Validators.required, Validators.minLength(6)]),
+             ],
+           });
+           break;
        }
        case 'invitado': {
-         this.email = 'invitado@gmail.com';
-         this.password = '222222';
+         this.validationsForm = this.formBuilder.group({
+           email: ['invitado@gmail.com',
+             Validators.compose([Validators.required, Validators.email])],
+           password: [
+            '222222',
+             Validators.compose([Validators.required, Validators.minLength(6)]),
+           ],
+         });
+         break;
+       }
+       case 'user': {
+         this.validationsForm = this.formBuilder.group({
+           email: ['usuario@gmail.com',
+             Validators.compose([Validators.required, Validators.email])],
+           password: [
+            '333333',
+             Validators.compose([Validators.required, Validators.minLength(6)]),
+           ],
+         });
+         break;
+       }
+       case 'anon': {
+          this.validationsForm = this.formBuilder.group({
+             email: ['anonimo@gmail.com',
+               Validators.compose([Validators.required, Validators.email])],
+             password: [
+              '444444',
+               Validators.compose([Validators.required, Validators.minLength(6)]),
+             ],
+           });
           break;
        }
-       case 'usuario': {
-         this.email = 'usuario@gmail.com';
-         this.password = '333333';
-          break;
-       }
-       case 'D': {
-          console.log('Poor');
+       case 'test': {
+         this.validationsForm = this.formBuilder.group({
+            email: ['tester@gmail.com',
+              Validators.compose([Validators.required, Validators.email])],
+            password: [
+             '555555',
+              Validators.compose([Validators.required, Validators.minLength(6)]),
+            ],
+          });
           break;
        }
        default: {
-          console.log('Invalid choice');
+          console.log(user);
           break;
        }
-    }
+     }
   }
-
 
 }
--- src/app/bienvenido/bienvenido.page.ts
+++ src/app/bienvenido/bienvenido.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope/ngx';
 import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 import { Flashlight } from '@ionic-native/flashlight/ngx';
@@ -20,21 +21,23 @@ export class BienvenidoPage implements OnInit {
   public accX: any;
   public accY: any;
   public accZ: any;
-  public gyrX: any;
-  public gyrY: any;
-  public gyrZ: any;
+  // public gyrX: any;
+  // public gyrY: any;
+  // public gyrZ: any;
   public promise: any;
   public promiseDos: any;
   public isVertial: any;
   public isHorizontal: any;
   public isLeft: any;
   public isRight: any;
-
+  public isOn: any;
   private options: GyroscopeOptions = {
     frequency: 1000
  };
 
   constructor(
+    private  router:  Router,
+    public afAuth: AngularFireAuth,
     db: AngularFirestore,
     private gyroscope: Gyroscope,
     private deviceMotion: DeviceMotion,
@@ -45,34 +48,32 @@ export class BienvenidoPage implements OnInit {
 
   ngOnInit() {
     this.isVertial = false;
-    this.isHorizontal = false;
+    this.isHorizontal = true;
     this.isLeft = false;
     this.isRight = false;
-    this.nativeAudio.preloadSimple('air-horn', 'assets/sounds/air-horn.wav');
-    this.nativeAudio.preloadSimple('bike', 'assets/sounds/bike-horn.wav');
-    this.nativeAudio.preloadSimple('sirena', 'assets/sounds/sirena.wav');
-    this.nativeAudio.preloadSimple('train', 'assets/sounds/train-low-horn.wav');
+    this.isOn = false;
+    this.cargarAudios();
   }
 
   // Giroscopio
-  gyrascope() {
-
-    this.gyroscope = new Gyroscope();
-
-    this.gyroscope.getCurrent()
-     .then((orientation: GyroscopeOrientation) => {
-
-      })
-     .catch();
-
-
-    this.promiseDos = this.gyroscope.watch(this.options)
-      .subscribe((orientation: GyroscopeOrientation) => {
-        this.gyrX = orientation.x;
-        this.gyrY = orientation.y;
-        this.gyrZ = orientation.z;
-      });
-  }
+  // gyrascope() {
+  //
+  //   this.gyroscope = new Gyroscope();
+  //
+  //   this.gyroscope.getCurrent()
+  //    .then((orientation: GyroscopeOrientation) => {
+  //
+  //     })
+  //    .catch();
+  //
+  //
+  //   this.promiseDos = this.gyroscope.watch(this.options)
+  //     .subscribe((orientation: GyroscopeOrientation) => {
+  //       this.gyrX = orientation.x;
+  //       this.gyrY = orientation.y;
+  //       this.gyrZ = orientation.z;
+  //     });
+  // }
 
   // Acelerometro
   Accelerometer() {
@@ -87,43 +88,50 @@ export class BienvenidoPage implements OnInit {
       this.promise = this.deviceMotion.watchAcceleration(this.options)
       .subscribe((acceleration: DeviceMotionAccelerationData) => {
 
-        this.accX = acceleration.x;
-        this.accY = acceleration.y;
-        this.accZ = acceleration.z;
+          this.accX = acceleration.x;
+          this.accY = acceleration.y;
+          this.accZ = acceleration.z;
 
-        // // Izquierda
-        // if ( acceleration.x > 2 && !this.isLeft) {
-        //   this.isLeft = true;
-        //   this.nativeAudio.play('sirena');
-        // }
-        //
-        // // Derecha
-        // if ( acceleration.x < -1 && !this.isRight) {
-        //   this.isRight = true;
-        //   this.nativeAudio.play('train');
-        // }
+          // // Izquierda
+          if ( acceleration.x >= 2 && !this.isLeft) {
+            this.isLeft = true;
+            this.nativeAudio.play('sirena');
+          }
 
-        // Vertical
-        if (acceleration.y > 8 && acceleration.z  < 2 && !this.isVertial && this.flashlight.available()) {
-          this.isVertial = true;
-          this.flashlight.switchOn();
-          this.nativeAudio.play('air-horn');
-          setTimeout(() => this.flashlight.switchOff(), 5000);
-        }
+          // Derecha
+          if ( acceleration.x <= -2 && !this.isRight) {
+            this.isRight = true;
+            this.nativeAudio.play('train');
+          }
 
-        // Horizontal
-        if (acceleration.y < 2 && acceleration.z  > 8 && !this.isHorizontal) {
-          this.isHorizontal = true;
-          this.vibration.vibrate(5000);
-          this.nativeAudio.play('bike');
-        }
+          // Vertical
+          if (acceleration.y > 8 && acceleration.z  < 2 && !this.isVertial && this.flashlight.available()) {
+            this.isVertial = true;
+            this.isHorizontal = false;
+            this.flashlight.switchOn();
+            this.nativeAudio.play('air-horn');
+            setTimeout(() => this.flashlight.switchOff(), 5000);
+          }
+
+          // Horizontal
+          if (acceleration.y < 2 && acceleration.z  > 8 && !this.isHorizontal) {
+            this.isHorizontal = true;
+            this.isVertial = false;
+            this.vibration.vibrate(5000);
+            this.nativeAudio.play('bike');
+          }
 
       });
   }
 
   Stop() {
-    this.promise.unsubscribe();
-    this.promiseDos.unsubscribe();
+
+    if (this.promise !== undefined) {
+      this.promise.unsubscribe();
+    }
+    if (this.promiseDos !== undefined) {
+        this.promiseDos.unsubscribe();
+    }
   }
 
   Clear() {
@@ -131,6 +139,34 @@ export class BienvenidoPage implements OnInit {
     this.isRight = false;
     this.isVertial = false;
     this.isHorizontal = false;
+    this.isOn = false;
+  }
+
+  logOut(): void {
+    this.afAuth.auth.signOut().then( () => {
+      this.nativeAudio.unload('air-horn');
+      this.nativeAudio.unload('bike');
+      this.nativeAudio.unload('sirena');
+      this.nativeAudio.unload('train');
+      this.router.navigateByUrl('login');
+    });
+  }
+
+  Iniciar() {
+    this.isOn = true;
+    this.Accelerometer();
+  }
+
+  Detener() {
+    this.Stop();
+    this.Clear();
+  }
+
+  cargarAudios() {
+    this.nativeAudio.preloadSimple('air-horn', 'assets/sounds/air-horn.wav');
+    this.nativeAudio.preloadSimple('bike', 'assets/sounds/bike-horn.wav');
+    this.nativeAudio.preloadSimple('sirena', 'assets/sounds/sirena.wav');
+    this.nativeAudio.preloadSimple('train', 'assets/sounds/train-low-horn.wav');
   }
 
 }
